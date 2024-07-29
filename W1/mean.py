e20bc24fd80ef4e56bae99833fc62eda551addd3
winiyaz <winiyaz@proton.me>
2024-07-29 10:26:33 -0700

    🙌 - PjSetup

--- W1/mean.py
+++ W1/mean.py
@@ -1,6 +1,17 @@
 # Working with the CSV File
 
 # Opening File
+# with open('we_da.csv') as df:
+# 	df = df.readlines()
+# 	print(df)
+
+# Using CSV Reader
+import csv
 with open('we_da.csv') as df:
-	df = df.readlines()
-	print(df)
+	data = csv.reader(df)
+	temp = []
+	print(data)
+	for r in data:
+		if r[1] != 'temp':
+			temp.append(int(r[1]))
+	print(temp)
