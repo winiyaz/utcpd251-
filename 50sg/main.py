fd740ca8f8f2714f3f0490611e38d975949c9188
winiyaz <winiyaz@proton.me>
2024-07-30 01:40:36 -0700

    🙌50sg - setup Coords

---
+++ 50sg/main.py
@@ -0,0 +1,25 @@
+# Game work is being done here
+import turtle
+from getcor import get_mouse_click_corr
+# Imports
+
+
+scr = turtle.Screen()
+scr.bgcolor("black")
+scr.setup(width=800, height=600)
+scr.title("USBootyMap")
+img = "bsg.gif"
+scr.addshape(img)
+
+turtle.shape(img)
+
+# Getting the coordinates in the image
+get_mouse_click_corr
+
+
+
+
+
+# Using Main loop
+# scr.exitonclick()
+turtle.mainloop()
